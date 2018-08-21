a886e3965286a4bc399a191ac461daaa543e625e
Arturo <arturoeslava88@gmail.com>
2018-08-21 18:35:53 +0000

    lesson 9

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ $(document).ready(function(){
 $("#contact-methods").css({border: "2px solid red"})
 .next().css({border:"2px solid green"})
 .closest("section").css({border: 2px solid blue})
+
+
+var tweet = "<div style='margin: 20px 0; padding: 10px; background: #eee'>The big fight live: Ham vs Cheese:</div>";
+
+$("#tweets div p").after(tweet);
