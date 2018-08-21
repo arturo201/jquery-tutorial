e7e087641b3e183da1d4336856b446847be84cb5
Arturo <arturoeslava88@gmail.com>
2018-08-21 14:16:57 +0000

    lesson 8

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     alert("jquery loaded");
 });
+
+
+$("#contact-methods").css({border: "2px solid red"})
+.next().css({border:"2px solid green"})
+.closest("section").css({border: 2px solid blue})
