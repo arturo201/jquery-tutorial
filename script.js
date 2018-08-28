272ff8782eede6d896a0b11af3c1b32b732c351b
Arturo <arturoeslava88@gmail.com>
2018-08-28 14:32:20 +0000

    lesson 18

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-$(window).load(function() {
-   
+$(document).ready("click", function(){
+   $("*").on("click", function(e){
+      console.log(e.target);
+      console.log("event type is! " + e.type);
+      console.log("x coordinates of the event is! " + e.pageX);
+      console.log("Y coordinates of the event is! " + e.pageY);
+      e.stopPropagation();
+   });
 });
