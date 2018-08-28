ba5c3fe421d9e99a7e6832e05f6f93d8da8b346a
Arturo <arturoeslava88@gmail.com>
2018-08-28 14:08:01 +0000

    lesson 16

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-var myLis = ("#points-of-sale li");
-
-myLis.on("click", function () {
-   $(this).css({"background" : "pink"});
-   
-   myLis.off("click");
-   
+$("$lead-banner").on("dblclick", function(){
+   alert("you double clicked me");
+   $("#lead-banner").off("dblclick");
 });
